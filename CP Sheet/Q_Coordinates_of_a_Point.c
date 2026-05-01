d247b5b613c6b4d993656e2331ad5d6311e56426
tasfiahsadia <tasfiahsadia@gmail.com>
2026-05-01 14:18:04 +0600

    Coordinates_of_a_Point

---
+++ CP Sheet/Q_Coordinates_of_a_Point.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    float x,y;
+    scanf("%f %f", &x, &y);
+    if(x > 0 && y > 0)
+        printf("Q1");
+    else if(x < 0 && y > 0)
+        printf("Q2");
+    else if(x < 0 && y < 0)
+        printf("Q3");
+    else if(x > 0 && y < 0)
+        printf("Q4");
+}
