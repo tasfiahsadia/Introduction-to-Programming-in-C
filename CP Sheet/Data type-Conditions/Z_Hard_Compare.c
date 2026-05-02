c2d9db2f3875851f3aef7621b71fa718b6c8ffcf
tasfiahsadia <tasfiahsadia@gmail.com>
2026-05-02 23:06:40 +0600

    Update 5th line

--- CP Sheet/Data type-Conditions/Z_Hard_Compare.c
+++ CP Sheet/Data type-Conditions/Z_Hard_Compare.c
@@ -2,6 +2,7 @@
 int main(){
     long long a,b,c,d;
     scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+    //(10^7)^(10^12) too big, so use log(10^7) = 7*log10 = 7 [here log10 = 1]
     if(b*log(a) > d*log(c))
         printf("YES");
     else
