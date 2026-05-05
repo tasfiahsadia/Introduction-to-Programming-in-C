5a4b5ac87f3fdd8aa45fbc7033d4094a159512fb
tasfiahsadia <tasfiahsadia@gmail.com>
2026-05-05 21:36:35 +0600

    Diamond Shape3

---
+++ Practice_18/W_Shape_3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    int star = 1;
+    for(int i=1; i<=n; i++){
+        for(int k=n-i; k>=1; k--)
+            printf(" ");
+        for(int j=1; j<=star; j++)
+            printf("*");
+        printf("\n");
+        star += 2;
+    }
+
+    star = 2*n-1;
+    for(int i=n; i>=1; i--){
+        for(int k=n-i; k>=1; k--)
+            printf(" ");
+        for(int j=1; j<=star; j++)
+            printf("*");
+        printf("\n");
+        star -= 2;
+    }
+}
