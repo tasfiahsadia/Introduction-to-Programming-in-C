2cdeac91294b77d26249ef3e83a9e7a22fa38f30
tasfiahsadia <tasfiahsadia@gmail.com>
2026-05-06 22:52:12 +0600

    Max and Min

---
+++ Practice_18/G_Max_and_MIN.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0; i<n; i++)
+        scanf("%d",&a[i]);
+    
+    int min = a[0], max = a[0];
+    for(int i=1; i<n; i++){
+        if(min > a[i])
+            min = a[i];
+        if(max < a[i])
+            max = a[i];
+    }
+    printf("%d %d", min, max);
+}
