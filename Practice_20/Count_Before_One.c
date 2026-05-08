46c6c7f666d29311c74749c47bba8482bcdb2ecd
tasfiahsadia <tasfiahsadia@gmail.com>
2026-05-09 01:25:53 +0600

    Count Before One

---
+++ Practice_20/Count_Before_One.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int count_before_one(int a[], int n){
+    int count = 0;  
+    for(int i=0; i<n; i++){
+        if(a[i] == 1)
+            break;
+        count++;
+    }
+    return count;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for(int i=0; i<n; i++){
+        scanf("%d", &a[i]);
+    } 
+    printf("%d", count_before_one(a, n));
+}
