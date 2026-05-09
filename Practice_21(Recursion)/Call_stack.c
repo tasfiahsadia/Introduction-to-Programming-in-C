810721d9dae57233cd5eae32447a9c1cb3cbee83
tasfiahsadia <tasfiahsadia@gmail.com>
2026-05-10 01:11:24 +0600

    Call Stack

---
+++ Practice_21(Recursion)/Call_stack.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void mello(){
+    printf("Mello\n");
+}
+void gello(){
+    printf("Gello\n");
+    mello();
+}
+void hello(){
+    printf("Hello\n");
+    gello();
+}
+int main(){
+    hello();
+}
