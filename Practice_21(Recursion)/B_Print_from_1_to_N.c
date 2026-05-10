e1ccd8ee41878ec381743c3764899de7c2c95500
tasfiahsadia <tasfiahsadia@gmail.com>
2026-05-10 14:39:20 +0600

    Print from 1 to N

---
+++ Practice_21(Recursion)/B_Print_from_1_to_N.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void print(int n, int i){
+    if(i > n)
+        return;
+    printf("%d\n", i);
+    print(n, i+1);
+}
+int main(){
+    int n;
+    scanf("%d", &n);
+    print(n,1);
+}
