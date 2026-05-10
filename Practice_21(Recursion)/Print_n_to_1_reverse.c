55dd772a05f32a36ea57c496f8cfc11764ab91e0
tasfiahsadia <tasfiahsadia@gmail.com>
2026-05-10 12:48:37 +0600

    Print N to 1 with Reverse (Recursio)

---
+++ Practice_21(Recursion)/Print_n_to_1_reverse.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void hello(int i){
+    if(i == 6)
+        return;
+    hello(i+1); //backtracking krte func. age call krte hbe
+    printf("%d\n", i); //backtracking er maddhome reverse print hoche
+}
+int main(){
+    hello(1);
+}
