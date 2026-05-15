cde39ae10c4009d9884f24288fc352bd65ed9ef6
tasfiahsadia <tasfiahsadia@gmail.com>
2026-05-15 22:47:38 +0600

    Updated

--- Practice_23/L_Summation.c
+++ Practice_23/L_Summation.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 
-long long sum(long long *a, int n){
+// long long sum(long long *a, int n){
+//     if (n == 0) 
+//         return 0;
+//     return a[0] + sum(a+1, n-1);
+// }
+
+// int main(){
+//     int n;
+//     scanf("%d", &n);
+//     long long a[1000];
+//     for (int i=0; i<n; i++)
+//         scanf("%lld", &a[i]);
+//     printf("%lld\n", sum(a, n));
+// }
+
+long long summation(long long a[], int n){
+    long long sum = 0;
     if (n == 0) 
         return 0;
-    return a[0] + sum(a+1, n-1);
+    for (int i=0; i<n; i++)
+        sum += a[i];
+    return sum;
 }
 
 int main(){
@@ -12,6 +30,5 @@ int main(){
     long long a[1000];
     for (int i=0; i<n; i++)
         scanf("%lld", &a[i]);
-    printf("%lld\n", sum(a, n));
-
+    printf("%lld\n", summation(a, n));
 }
