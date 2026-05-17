a2850137ac66c796a98ac9f83f6aa918c57d9a21
tasfiahsadia <tasfiahsadia@gmail.com>
2026-05-17 20:34:34 +0600

    Search In Matrix

---
+++ Practice_23/S_Search_In_Matrix.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int n,m;
+    scanf("%d %d", &n, &m);
+    int a[n][m];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++)
+            scanf("%d", &a[i][j]);
+    }
+
+    int x;
+    scanf("%d", &x);
+    
+    int found = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(a[i][j] == x){
+                found = 1;
+                break;
+            }
+        }
+    }
+
+    if(found == 1)
+        printf("will not take number");
+    else
+        printf("will take number");
+}
